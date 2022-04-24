c6384e56539f14cf598f3887efbe536e0df1487e
JustifiedTech <kcsimpleslim@gmail.com>
2022-04-24 19:54:52 +0100

    Update transaction_bloc.dart

--- lib/bloc_providers/transaction_bloc.dart
+++ lib/bloc_providers/transaction_bloc.dart
@@ -9,14 +9,14 @@ part 'transaction_event.dart';
 part 'transaction_state.dart';
 
 class TransactionBloc extends Bloc<TransactionEvent, TransactionState> {
-  final TransactionRepository _transactionRepository;
+  final TransactionRepository transactionRepository;
 
-  TransactionBloc(this._transactionRepository)
+  TransactionBloc({required this.transactionRepository})
       : super(TransactionLoadingState()) {
     on<LoadTransactionEvent>((event, emit) async {
       emit(TransactionLoadingState());
       try {
-        final transaction = await _transactionRepository.readJson();
+        final transaction = await transactionRepository.readJson();
         emit(TransactionLoadedState(transaction));
       } catch (e) {
         emit(TransactionErrorState(e.toString()));
