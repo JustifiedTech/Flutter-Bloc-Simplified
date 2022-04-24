2c519711a61cd7c5c26ab2546edebd8a608b3e2a
JustifiedTech <kcsimpleslim@gmail.com>
2022-04-24 19:52:54 +0100

    refactor ios

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:new_app/main.dart';
+import 'package:new_app/repository/posts_repository.dart';
+import 'package:new_app/service/post_service.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester
+        .pumpWidget(MyApp(postsRepository: PostsRepository(PostService())));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
