5d98fa37f052e9600782180c98ab04b334df7b5d
JustifiedTech <kcsimpleslim@gmail.com>
2022-04-24 19:55:05 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:new_app/utils/routes.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:new_app/bloc_providers/bloc_navigator.dart';
+import 'package:new_app/models/post.dart';
+import 'package:new_app/repository/posts_repository.dart';
+import 'package:new_app/service/post_service.dart';
+import 'package:new_app/user_interface/posts_view.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'bloc_providers/cubit/nav_cubit.dart';
+import 'bloc_providers/cubit/posts_cubit.dart';
+import 'bloc_providers/transaction_bloc.dart';
+import 'repository/transaction_repository.dart';
+import 'utils/routes.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //   await DatabaseHelper.initDb();
+  // await GetStorage.init();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(MyApp(postsRepository: PostsRepository(PostService())));
+
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  const MyApp({Key? key, required this.postsRepository}) : super(key: key);
+  final PostsRepository postsRepository;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -17,7 +37,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      initialRoute: AppRoute.allTransactions,
+      // home: const AllTransactions(),
+      home: MultiBlocProvider(providers: [
+        BlocProvider(create: (context) => NavCubit<Posts>()),
+        BlocProvider(create: (context) => PostsCubit(postsRepository)),
+      ], child: const BlocNavigator()),
+      // initialRoute: AppRoute.allTransactions,
       onGenerateRoute: AppRoute.onGenerateRoute,
     );
   }
