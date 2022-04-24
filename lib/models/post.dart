31d401d375467f12b1d36c9629fdc440ac726c2a
JustifiedTech <kcsimpleslim@gmail.com>
2022-04-24 19:55:10 +0100

    Create post.dart

---
+++ lib/models/post.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+List<Posts> postsFromJson(String str) =>
+    List<Posts>.from(json.decode(str).map((x) => Posts.fromJson(x)));
+
+String postsToJson(List<Posts> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Posts {
+  int? userId;
+  int? id;
+  String? title;
+  String? body;
+
+  Posts({this.userId, this.id, this.title, this.body});
+
+  Posts.fromJson(Map<String, dynamic> json) {
+    userId = json['userId'];
+    id = json['id'];
+    title = json['title'];
+    body = json['body'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['userId'] = userId;
+    data['id'] = id;
+    data['title'] = title;
+    data['body'] = body;
+    return data;
+  }
+}
