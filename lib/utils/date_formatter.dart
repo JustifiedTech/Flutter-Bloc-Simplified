abfe42af7f47ef311378546631de79277791fcd4
JustifiedTech <kcsimpleslim@gmail.com>
2022-04-24 19:56:11 +0100

    Update date_formatter.dart

--- lib/utils/date_formatter.dart
+++ lib/utils/date_formatter.dart
@@ -8,18 +8,34 @@ class DateFormatter {
   }
 
   static String toDay(DateTime dateTime) {
-    return DateFormat.yMMMd().format(dateTime);
-  }
+    final day = DateFormat.d().format(dateTime);
+    final date = DateFormat.yMMM().format(dateTime);
 
+    return '${36}'.formatDate + ' $date';
+  }
 
   static DateTime? parseToDateTime(String dateTime) {
     return DateTime.tryParse(dateTime);
   }
 
-
   static String parseToDay(String dateTime) {
     var result = parseToDateTime(dateTime);
-    return toDay(result!);
+    return toDay(result ?? DateTime.now());
   }
+}
 
+extension DateExtendion on String {
+  get formatDate {
+    if (this == '1' || (length > 1 && substring(1, 2) == '1')) {
+      return this + 'st';
+    }
+    if (this == '2' || (length > 1 && substring(1, 2) == '2')) {
+      return this + 'nd';
+    }
+    if (this == '3' || (length > 1 && substring(1, 2) == '3')) {
+      return substring(0, 1);
+    } else {
+      return this + 'th';
+    }
+  }
 }
